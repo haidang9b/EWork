89fcb1297def88794cde8a665cfd86456a036ebc
Phan Hai Dang <drom97977@gmail.com>
2022-10-26 22:53:02 +0700

    UI Login page

---
+++ Source/EW.UI/src/pages/Login/index.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import {
+    CardActions,
+    CardContent,
+    Divider,
+    Grid,
+    TextField,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { SendSharp } from "@mui/icons-material";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import GoogleIcon from "@mui/icons-material/Google";
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = () => {
+        console.log(username, password);
+    };
+
+    const handleLoginGoogle = () => {
+        console.log("Login with google");
+    };
+    return (
+        <Container>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+            >
+                <Card
+                    sx={{
+                        width: { sx: "100%", sm: "100%", md: "50%" },
+                    }}
+                    style={{
+                        padding: "8px",
+                    }}
+                >
+                    <CardContent>
+                        <Typography variant="h3" gutterBottom>
+                            Login
+                        </Typography>
+                        <Box pb={2}>
+                            <TextField
+                                label="Username"
+                                variant="standard"
+                                fullWidth
+                                required
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <TextField
+                                label="Password"
+                                variant="standard"
+                                fullWidth
+                                required
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Box>
+                    </CardContent>
+                    <CardActions>
+                        <Stack minWidth="100%">
+                            <Button
+                                type="button"
+                                variant="contained"
+                                startIcon={<SendSharp />}
+                                onClick={handleSubmit}
+                                sx={{
+                                    minWidth: "100%",
+                                }}
+                            >
+                                Login
+                            </Button>
+                            <br />
+                            <Divider>Login with</Divider>
+                            <br />
+                            <Button
+                                type="button"
+                                variant="outlined"
+                                startIcon={<GoogleIcon />}
+                                onClick={handleLoginGoogle}
+                                sx={{
+                                    minWidth: "100%",
+                                }}
+                            >
+                                Google
+                            </Button>
+                        </Stack>
+                    </CardActions>
+                </Card>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Login;
