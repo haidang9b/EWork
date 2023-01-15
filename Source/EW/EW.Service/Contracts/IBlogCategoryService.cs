d0f8a8e58c49ac8bbd2f6f844e8ba3b2584d266a
Phan Hải Đăng <drom97977@gmail.com>
2023-01-15 15:28:32 +0700

    Add: Service category

---
+++ Source/EW/EW.Service/Contracts/IBlogCategoryService.cs
@@ -0,0 +1,17 @@
+﻿using EW.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EW.Services.Contracts
+{
+    public interface IBlogCategoryService
+    {
+        Task<BlogCategory> Add(BlogCategory model);
+        Task<BlogCategory> Update(BlogCategory model);
+        Task<bool> Delete(BlogCategory model);
+        Task<IEnumerable<BlogCategory>> GetAll();
+    }
+}
---
+++ Source/EW/EW.Service/Contracts/IBlogService.cs
@@ -0,0 +1,17 @@
+﻿using EW.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EW.Services.Contracts
+{
+    public interface IBlogService
+    {
+        Task<IEnumerable<Blog>> GetAll();
+        Task<Blog> Add(Blog blog);
+        Task<Blog> Update(Blog blog);
+        Task<bool> Delete(Blog blog);
+    }
+}
