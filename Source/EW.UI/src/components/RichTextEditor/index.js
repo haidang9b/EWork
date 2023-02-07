774adfb5f018639a1ffb50e481e45aeaa945fdb6
Phan Hải Đăng <drom97977@gmail.com>
2023-02-07 23:25:38 +0700

    User can upload image to baseurl64

--- Source/EW.UI/src/components/RichTextEditor/index.js
+++ Source/EW.UI/src/components/RichTextEditor/index.js
@@ -11,6 +11,21 @@ import "./RichTextEditor.css";
  * @returns Component RichTextEditor
  */
 const RichTextEditor = ({ editor, setEditor, initialHTML }) => {
+    const toolbarOptions = [
+        ["bold", "italic", "underline", "strike"], // toggled buttons
+        ["blockquote", "code-block"],
+        [{ header: 1 }, { header: 2 }], // custom button values
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+        [{ direction: "rtl" }], // text direction
+        [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ["link", "image"], // add's image support
+        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+        [{ font: [] }],
+        [{ align: [] }],
+        ["clean"], // remove formatting button
+    ];
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return;
         wrapper.innerHTML = "";
@@ -18,6 +33,9 @@ const RichTextEditor = ({ editor, setEditor, initialHTML }) => {
         wrapper.append(editorTag);
         const q = new Quill(editorTag, {
             theme: "snow",
+            modules: {
+                toolbar: toolbarOptions,
+            },
         });
         setEditor(q);
         // eslint-disable-next-line
