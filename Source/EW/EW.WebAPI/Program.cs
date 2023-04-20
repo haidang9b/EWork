5667d6e3887b939bf960d64ede27444203ef694b
Phan Hải Đăng <drom97977@gmail.com>
2023-04-20 17:42:59 +0700

    Add: Extension method for register service

--- Source/EW/EW.WebAPI/Program.cs
+++ Source/EW/EW.WebAPI/Program.cs
@@ -7,6 +7,7 @@ using EW.Services.Business;
 using EW.Services.Constracts;
 using EW.Services.Contracts;
 using EW.WebAPI;
+using EW.WebAPI.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
@@ -26,10 +27,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy(MyAllowSpecificOrigins, builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 });
 
-RegisterService(builder.Services);
+builder.Services.RegisterService();
 builder.Services.Configure<EmailConfig>(builder.Configuration.GetSection("MailSettings"));
 builder.Services.Configure<CustomConfig>(builder.Configuration.GetSection("CustomConfigs"));
 builder.Services.AddCors();
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
@@ -83,28 +85,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-void RegisterService(IServiceCollection services)
-{
-    IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
-    services.AddSingleton(mapper);
-    services.AddScoped(typeof(IRepository<>), typeof(EWRepository<>));
-    services.AddScoped(typeof(IDatabaseFactory<>), typeof(DatabaseFactory<>));
-    services.AddScoped<IUserService, UserService>();
-    services.AddScoped<ITokenService, TokenService>();
-    services.AddScoped<IRecruiterService, RecruiterService>();
-    services.AddScoped<IUserCVService, UserCVService>();
-    services.AddScoped<IEmailService, EmailService>();
-    services.AddScoped<IRecruitmentPostService, RecruitmentPostService>();
-    services.AddScoped<ICompanyService, CompanyService>();
-    services.AddScoped<IApplicationService, ApplicationService>();
-    services.AddScoped<IProfileSerivce, ProfileService>();
-    services.AddScoped<IWorkHistoryService, WorkHistoryService>();
-    services.AddScoped<IProjectService, ProjectService>();
-    services.AddScoped<ICertificateService, CertificateService>();
-    services.AddScoped<IEducationService, EducationService>();
-    services.AddScoped<IChartService, ChartService>();
-    services.AddScoped<IBlogCategoryService, BlogCategoryService>();
-    services.AddScoped<IBlogService, BlogService>();
-    services.AddScoped<IUnitOfWork, UnitOfWork>();
-}
