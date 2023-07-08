3a8506a9bb78ee6e921d486576395621187d4803
Phan Hải Đăng <drom97977@gmail.com>
2023-07-08 17:23:18 +0700

    Add : ISerializable

--- Source/EW/EW.Commons/Exceptions/EWException.cs
+++ Source/EW/EW.Commons/Exceptions/EWException.cs
@@ -1,7 +1,11 @@
-﻿namespace EW.Commons.Exceptions
+﻿using System.Runtime.Serialization;
+
+namespace EW.Commons.Exceptions
 {
-    public class EWException : Exception
+    public class EWException : Exception, ISerializable
     {
         public EWException(string message) : base(message) { }
+
+        protected EWException(SerializationInfo serializations, StreamingContext context) : base(serializations, context) { }
     }
 }
