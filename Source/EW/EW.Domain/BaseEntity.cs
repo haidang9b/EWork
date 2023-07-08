c605e8f1dfd038089206e2bf475cbd6903134133
Phan Hải Đăng <drom97977@gmail.com>
2023-07-08 18:48:26 +0700

    remove: ew entity

--- Source/EW/EW.Domain/BaseEntity.cs
+++ Source/EW/EW.Domain/BaseEntity.cs
@@ -1,14 +1,9 @@
 ﻿namespace EW.Domain
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         public long Id { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
-    }
-
-    public abstract class EWEntity : BaseEntity
-    {
-
     }
 }
