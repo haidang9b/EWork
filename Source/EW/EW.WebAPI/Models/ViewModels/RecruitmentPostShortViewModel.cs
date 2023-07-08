bc22d3c65fd052bd8a8dcb7e7c8f8ee89481902d
Phan Hải Đăng <drom97977@gmail.com>
2023-07-08 12:32:02 +0700

    refactor: move code from program to extension method

--- Source/EW/EW.WebAPI/Models/ViewModels/RecruitmentPostShortViewModel.cs
+++ Source/EW/EW.WebAPI/Models/ViewModels/RecruitmentPostShortViewModel.cs
@@ -5,18 +5,18 @@ namespace EW.WebAPI.Models.ViewModels
     public class RecruitmentPostShortViewModel
     {
         public long Id { get; set; }
-        public string JobTitle { get; set; }
+        public string? JobTitle { get; set; }
         public ESalaryType SalaryType { get; set; }
         public int SalaryFrom { get; set; }
         public int SalaryTo { get; set; }
         public ECurrency Currency { get; set; }
-        public string TechStacks { get; set; }
+        public string? TechStacks { get; set; }
         public double YearExperience { get; set; }
-        public string JobDescription { get; set; }
+        public string? JobDescription { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
-        public string AvatarUrl { get; set; }
+        public string? AvatarUrl { get; set; }
         public EWorkingType WorkingType { get; set; }
         public ECompanyType CompanyType { get; set; }
-        public string CompanyName { get; set; }
+        public string? CompanyName { get; set; }
     }
 }
